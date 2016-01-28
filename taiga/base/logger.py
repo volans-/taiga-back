1174892a70024e03063087790576db7c5e14136f
Jesús Espino <jespinog@gmail.com>
2016-01-28 10:55:34 +0100

    WIP

--- taiga/base/logger.py
+++ taiga/base/logger.py
@@ -7,10 +7,15 @@ bi_logger = logging.getLogger("bi")
 def bilogger(user, view, **kwargs):
     data = copy.copy(kwargs)
 
+    if "type" not in kwargs:
+        data["type"] = "ok"
+
     data['user'] = 'anon'
     if not user.is_anonymous():
         data['user'] = user.id
     data['view'] = view.get_view_name()
-    data['action'] = view.action
+
+    if "action" not in kwargs:
+        data['action'] = view.action
 
     bi_logger.info(json.dumps(data))
